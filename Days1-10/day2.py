c35467654949699a0d237d8250dd4c61e3182180
JoshuaFarara <joshua.farara@gmail.com>
2023-05-06 23:34:49 -0400

    day2 Commit 1
    Changes to be committed:
    	new file:   Days1-10/day2.py

---
+++ Days1-10/day2.py
@@ -0,0 +1,32 @@
+'''
+Day One of 100 Days of Code
+
+May 6th, 2023
+
+Todays program will create something out of the basics. Lets tell a story today through code. 
+How Artistic can you be with your programming code? Lets just have fun, relax, and code!
+
+####  Name Spaces   ####
+Directories with different names, specify absolute path to the file. 
+
+Understanding namespace helps you improve your uderstanding of structure within development. 
+Its about access within a program. What objects can be accessed amongst others. 
+
+WHen a user creates a module, a global namespace getes created, later the creation of local functions creates local namespace.
+built-in namespace encompasses global namespace an dthe gloabal namespace encompasses the local namespace. 
+'''
+
+# variable is in the global namespace
+moth1 = 10
+def moth_it():
+    # var2 is in the local namespace
+    moth2 = 11
+    def moth_it_inner(): # inner function
+
+        #var3 is int he nested local namespace
+        moth3 = 12
+
+
+# Scope
+# Access is addressed in the section. Allowed by the scope of the object. 
+
