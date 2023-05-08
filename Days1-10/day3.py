256ab5f770d4117d7643f3708525f2b38603e0db
JoshuaFarara <joshua.farara@gmail.com>
2023-05-07 23:55:49 -0400

    Changes to be committed: 	modified:   Days1-10/day3.py fixed the date

--- Days1-10/day3.py
+++ Days1-10/day3.py
@@ -1,7 +1,7 @@
 '''
 Day Three of 100 Days of Code
 
-May 6th, 2023
+May 7th, 2023
 
 Todays program will create something out of the basics. Lets tell a story today through code. 
 How Artistic can you be with your programming code? Lets just have fun, relax, and code!
