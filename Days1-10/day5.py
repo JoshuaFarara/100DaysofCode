c261c68ad99fad1bedbb26e7a3832ec3c9202cb9
JoshuaFarara <joshua.farara@gmail.com>
2023-05-09 23:58:08 -0400

    Changes to be committed: 	modified:   Days1-10/day5.py

--- Days1-10/day5.py
+++ Days1-10/day5.py
@@ -1,7 +1,7 @@
 '''
 Day four of 100 Days of Code
 
-May 8th, 2023
+May 9th, 2023
 
 Todays program will create something out of the basics. Lets tell a story today through code. 
 How Artistic can you be with your programming code? Lets just have fun, relax, and code!
