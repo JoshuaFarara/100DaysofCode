130507d9da748c71c426d69b6ff4ca4d30abbc81
JoshuaFarara <joshua.farara@gmail.com>
2023-05-16 00:11:26 -0400

    Changes to be committed: 	new file:   Days1-10/day6.py 	new file:   Days1-10/day8.py 	modified:   daysDays.md

---
+++ Days1-10/day6.py
@@ -0,0 +1,18 @@
+'''
+Day six of 100 Days of Code
+
+May 16th, 2023
+
+Todays program will create something out of the basics. Lets tell a story today through code. 
+How Artistic can you be with your programming code? Lets just have fun, relax, and code!
+
+
+Python Lists
+Python Tuples
+Python Sets
+Python Dictionary
+Python Arrays
+
+
+'''
+
---
+++ Days1-10/day8.py
@@ -0,0 +1,19 @@
+'''
+Day Eight of 100 Days of Code
+
+May 12th, 2023
+
+Todays program will create something out of the basics. Lets tell a story today through code. 
+How Artistic can you be with your programming code? Lets just have fun, relax, and code!
+
+
+# Todays Schedule
+Python Lists
+Python Tuples
+Python Sets
+
+'''
+
+# creating lists
+bladed_moths = ["Red", "Green", "Blue", "Black", "White"]
+print(bladed_moths[1])
