b543737166bbd0c86a30b9a74d259795ed34d972
lyfjourney <joshua.farara@gmail.com>
2023-06-27 23:57:07 -0400

    Changes to be committed: 	modified:   Days21-40/day21.py

--- Days21-40/day21.py
+++ Days21-40/day21.py
@@ -11,3 +11,18 @@ How Artistic can you be with your programming code? Lets just have fun, relax, a
 Arrays
 
 '''
+arr = [10, 20, 30]  # This array will store integer
+arr2 = ['c', 'd', 'e']  # This array will store characters
+arr3 = [28.5, 36.5, 40.2]  # This array will store floating elements
+
+
+# list of integers
+my_list = [1, 2, 3, 4]
+  
+# Empty list
+my_list = []
+  
+# list of mixed data types
+my_list = ["Hello", 1, 5.5]
+
+# 	Write a program to reverse the array
