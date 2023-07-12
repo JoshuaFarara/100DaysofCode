dab002700faa747a3d3bfe4baa86bfd09fe546c0
lyfjourney <joshua.farara@gmail.com>
2023-07-12 00:11:47 -0400

    Changes to be committed: 	new file:   Days21-40/day31.py
    Changes not staged for commit:
    	modified:   Days21-40/day30.py
    	modified:   README.md

---
+++ Days21-40/day31.py
@@ -0,0 +1,29 @@
+'''
+Day Thirty-First of 100 Days of Code
+
+July 12th, 2023
+
+Todays program demonstrates the implementation of a priority queue using an array.
+
+# Todays Schedule represented by *
+
+*Queues
+    How to Implement Priority Queue? 
+    Priority queue can be implemented using the following data structures: 
+        Arrays
+        *Linked list
+        Heap data structure
+        Binary search tree
+Linked List
+    singly
+    doubly
+Trees
+Tries
+Hashing
+Heaps
+Graphs (BFS and DFS)
+Sorting
+Searching
+'''
+
+# Implement Priority Queue Using Linked List
