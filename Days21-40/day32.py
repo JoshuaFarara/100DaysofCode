aaa2c5036c9e010bad5ece6b7dd4d4426efe73be
lyfjourney <joshua.farara@gmail.com>
2023-07-17 23:55:29 -0400

    Changes to be committed: 	new file:   Days21-40/day32.py

---
+++ Days21-40/day32.py
@@ -0,0 +1,22 @@
+'''
+Day Thirty-Second of 100 Days of Code
+
+July 17th, 2023
+
+Todays program demonstrates the implementation of a priority queue using an array.
+
+# Todays Schedule represented by *
+
+Queues
+Linked List
+Trees
+Tries
+Hashing
+*Heaps
+Graphs (BFS and DFS)
+Sorting
+Searching
+'''
+
+# Heaps
+# A Heap is a special Tree-based data structure in which the tree is a complete binary tree.
