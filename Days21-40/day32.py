b35e2f17b325a26942356979593d0c8b8e375c47
lyfjourney <joshua.farara@gmail.com>
2023-07-21 01:23:19 -0400

    modified:   Days21-40/day32.py 	new file:   Days21-40/day33.py

--- Days21-40/day32.py
+++ Days21-40/day32.py
@@ -27,7 +27,7 @@ data = [5, 6, 12, 23, 1, 17]
 
 #converts the example list into heap
 heapq.heapify(data)
-print("the crerated heap is: ", (list(data)))
+print("The created heap is: ", (list(data)))
 
 '''
 The structure explained
@@ -36,3 +36,27 @@ i           is the first node, at index i
 2*i+2       the second child is at the index (2*i+2)
 (i -1)/2    the parent node
 '''
+# using heappush() to push elements intop heap, pushes 36
+heapq.heappush(data, 36)
+
+print("The modified heap after push is: ", end="")
+print(list(data))
+
+# # using heappop() to pop smallest element 
+print("The popped and smallest element is: ", end="")
+print(heapq.heappop(data))
+print(list(data))
+
+#can append and pop simultaneously with heappushpop(heap, ele) ex. ->
+print("The popped item using heappushpop() is : ", end="")
+print(heapq.heappushpop(data, 8))
+print(list(data))
+
+# using heapreplace() to push and pop items simultaneously
+print("The popped item using heapreplace() is : ", end="")
+print(heapq.heapreplace(data, 4))
+print(list(data))
+
+# nlargest(k, iterable, key = fun): This function is used to return the k largest elements from the iterable specified and satisfy the key if mentioned.
+# nsmallest(k, iterable, key = fun): This function is used to return the k smallest elements from the iterable specified and satisfy the key if mentioned.
+
---
+++ Days21-40/day33.py
@@ -0,0 +1,21 @@
+'''
+Day Thirty-Third of 100 Days of Code
+
+July 20th, 2023
+
+Todays program demonstrates the implementation of a priority queue using an array.
+
+# Todays Schedule represented by *
+
+Queues
+Linked List
+Trees
+Tries
+Hashing
+Heaps
+Graphs (BFS and DFS)
+*Sorting
+Searching
+'''
+
+# Callable Function in Python
