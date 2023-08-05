a62399a56d0d5811a2a5ee53078e1b5aef75421a
lyfjourney <joshua.farara@gmail.com>
2023-08-05 17:13:11 -0400

    Changes to be committed: 	modified:   PyhtonPrograms/BasicPrograms/mergeArrays.py

--- PyhtonPrograms/BasicPrograms/mergeArrays.py
+++ PyhtonPrograms/BasicPrograms/mergeArrays.py
@@ -1,8 +1,46 @@
-import array
-#merge arrays problem
-arr1 = [1, 6, 3, 9, 4 ]
+# import array
+# #merge arrays problem
+# arr1 = [1, 6, 3, 9, 4 ]
 
-arr2 = [21, 48, 12, 32, 19]
+# arr2 = [21, 48, 12, 32, 19]
 
-print(arr1.sort())
-# fullArr
+# print(arr1.sort())
+# # fullArr
+
+
+# Python program to merge two sorted arrays/
+def mergeArrays(arr1, arr2, n1, n2, arr3):
+    i = 0
+    j = 0
+    k = 0
+ 
+    # traverse the arr1 and insert its element in arr3
+    while(i < n1):
+        arr3[k] = arr1[i]
+        k += 1
+        i += 1
+ 
+    # now traverse arr2 and insert in arr3
+    while(j < n2):
+        arr3[k] = arr2[j]
+        k += 1
+        j += 1
+ 
+    # sort the whole array arr3
+    arr3.sort()
+ 
+ 
+# Driver code
+if __name__ == '__main__':
+    arr1 = [1, 3, 5, 7]
+    n1 = len(arr1)
+ 
+    arr2 = [2, 4, 6, 8]
+    n2 = len(arr2)
+ 
+    arr3 = [0 for i in range(n1+n2)]
+    mergeArrays(arr1, arr2, n1, n2, arr3)
+ 
+    print("Array after merging")
+    for i in range(n1 + n2):
+        print(arr3[i], end=" ")
