d999b67df550e12b58063076b6509c4b9e81fe6e
lyfjourney <joshua.farara@gmail.com>
2023-08-31 22:55:46 -0400

    Changes to be committed: 	modified:   PyhtonPrograms/ArrayPrograms/duplicatesArray.py 	modified:   days21_40/day34.py

--- days21_40/day34.py
+++ days21_40/day34.py
@@ -1,14 +1,33 @@
-fruits = ['apple', 'orange', 'apple']
+'''
+Day Thirty-Fourth of 100 Days of Code
 
-def count_fruits(fruits):
-  '''
-  input: ['apple', 'orange', 'apple']
-  output: {'apple':2, 'orange':1}
-  '''
-  out = {}
-  for fruit in fruits:
-    if fruit not in out:
-      out[fruit] = 1
-    else:
-      out[fruit] += 1
-    return out
+August 31st, 2023
+
+Todays program demonstrates the implementation of a priority queue using an array.
+
+# Todays Schedule represented by *
+
+Exception handling
+User Input Exception HAndling
+try and except
+
+SyntaxError: This exception is raised when the interpreter encounters a syntax error in the code, such as a misspelled keyword, a missing colon, or an unbalanced parenthesis.
+TypeError: This exception is raised when an operation or function is applied to an object of the wrong type, such as adding a string to an integer.
+NameError: This exception is raised when a variable or function name is not found in the current scope.
+IndexError: This exception is raised when an index is out of range for a list, tuple, or other sequence types.
+KeyError: This exception is raised when a key is not found in a dictionary.
+ValueError: This exception is raised when a function or method is called with an invalid argument or input, such as trying to convert a string to an integer when the string does not represent a valid integer.
+AttributeError: This exception is raised when an attribute or method is not found on an object, such as trying to access a non-existent attribute of a class instance.
+IOError: This exception is raised when an I/O operation, such as reading or writing a file, fails due to an input/output error.
+ZeroDivisionError: This exception is raised when an attempt is made to divide a number by zero.
+ImportError: This exception is raised when an import statement fails to find or load a module.
+'''
+
+# practice
+a = [1, 2, 3]
+try:
+    print("Second element = %d" %(a[1]))
+
+    print("Fourth element = %d" %(a[3]))
+except:
+    print ("An error occured.")
